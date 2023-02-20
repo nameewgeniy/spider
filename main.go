ab2d8bc3a9abff50b9396acb9b54998c866cd900
Evgeny Nakapyuk <nameewgeniy@ya.ru>
2023-02-21 01:43:31 +0300

    run remote browser

--- main.go
+++ main.go
@@ -2,18 +2,8 @@ package main
 
 import (
 	"context"
-	"database/sql"
-	"encoding/json"
 	"fmt"
-	"github.com/ClickHouse/clickhouse-go"
 	"github.com/chromedp/chromedp"
-	"github.com/geziyor/geziyor"
-	"github.com/geziyor/geziyor/client"
-	"io/ioutil"
-	"log"
-	"regexp"
-	"strconv"
-	"strings"
 )
 
 type WProduct struct {
@@ -37,64 +27,23 @@ type Product struct {
 
 func main() {
 
-	connect, err := sql.Open("clickhouse", "tcp://127.0.0.1:9900?debug=true")
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := connect.Ping(); err != nil {
-		if exception, ok := err.(*clickhouse.Exception); ok {
-			fmt.Printf("[%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
-		} else {
-			fmt.Println(err)
-		}
-		return
-	}
+	allocatorContext, cancel := chromedp.NewRemoteAllocator(context.Background(), "http://browser:9222")
+	defer cancel()
 
-	ctx, cancel := chromedp.NewContext(context.Background())
+	// create context
+	ctx, cancel := chromedp.NewContext(allocatorContext)
 	defer cancel()
 
 	var res string
 
-	err = chromedp.Run(ctx,
-		chromedp.Navigate(`https://www.wildberries.ru/catalog/18840845/detail.aspx`),
-		chromedp.Click("#a-Comments", chromedp.ByQuery),
-		chromedp.WaitVisible(`.comment`),
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(`https://mk.ru`),
 		chromedp.OuterHTML("body", &res, chromedp.ByQuery),
-		//chromedp.OuterHTML("#content", &outerAfter, chromedp.ByQuery),
 	)
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	err = ioutil.WriteFile("var/index.html", []byte(res), 0)
-
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-}
-
-func parseMovies(g *geziyor.Geziyor, r *client.Response) {
-
-	//rePageType := regexp.MustCompile(`"PageType":.*"?`)
-	reProductType := regexp.MustCompile(`(?s)google_tag_params = {.*?}`)
-	result := reProductType.Find(r.Body)
-	jsonData := strings.Replace(string(result), "google_tag_params = ", "", 1)
-
-	wp := WProduct{}
-
-	json.Unmarshal([]byte(jsonData), &wp)
-
-	price, _ := strconv.Atoi(wp.Value[0])
-
-	p := Product{
-		Name:     wp.Ptype[0],
-		Brand:    wp.Pbrand,
-		ID:       wp.ProdID[0],
-		Discount: wp.Discount,
-		Delivery: wp.Delivery,
-		Price:    uint(price),
-	}
-
-	fmt.Println(p)
+	fmt.Println(res)
 }
