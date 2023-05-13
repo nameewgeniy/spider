c049e0ad3159d45a7891129313dcaf6e29c3dab6
Evgeny Nakapyuk <nameewgeniy@ya.ru>
2023-05-13 08:20:06 +0300

    step

--- app/app.js
+++ app/app.js
@@ -1,41 +1,22 @@
-const express = require('express')
-require('dotenv').config();
+import { Parser } from './parser.js';
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
 const port = process.env.PORT || 3000
-const puppeteer = require('puppeteer-extra')
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')
-puppeteer.use(StealthPlugin())
-const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker')
-puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
-
-
 const app = express()
 
 app.get('/:domain', (req, res) => {
 
-  puppeteer.launch({
-    bindAddress: "0.0.0.0",
-    args: [
-      "--headless", 
-      "--no-sandbox",
-      "--disable-gpu",
-      "--disable-dev-shm-usage",
-      "--remote-debugging-port=9222",
-      "--remote-debugging-address=0.0.0.0"
-    ]
-  }).then(async browser => {
-  
-    const page = await browser.newPage()
-    await page.setViewport({ width: 800, height: 600 })
-  
-    console.log(`Testing adblocker plugin..`)
-    await page.goto('https://' + req.params.domain)
-    await page.waitForTimeout(1000)
-    const content = await page.content()
-    res.send(content)
-  
-    await browser.close()
-  })
+  const pr = new Parser()
 
+  async fn => {
+    content = await pr.Parse('http://mk.ru')
+    res.send(content)
+  }
+
+  fn()
   
 })
 
--- app/parser.js
+++ app/parser.js
@@ -1,9 +1,41 @@
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import  AdblockerPlugin from 'puppeteer-extra-plugin-adblocker';
+
+
 export class Parser {
-    constructor(name){
-        this.name = name
+    constructor(){
+        
     }
 
-    SayName() {
-        console.log(this.name)
+    Parse(url) {
+
+        puppeteer.use(StealthPlugin())
+        puppeteer.use(AdblockerPlugin({ blockTrackers: true }))
+
+        puppeteer.launch({
+            bindAddress: "0.0.0.0",
+            args: [
+              "--headless", 
+              "--no-sandbox",
+              "--disable-gpu",
+              "--disable-dev-shm-usage",
+              "--remote-debugging-port=9222",
+              "--remote-debugging-address=0.0.0.0"
+            ]
+          }).then(async browser => {
+          
+            const page = await browser.newPage()
+            await page.setViewport({ width: 800, height: 600 })
+          
+            await page.goto(url)
+            await page.waitForTimeout(1000)
+
+            const content = await page.content()
+          
+            await browser.close()
+
+            return content
+          })
     }
 }
